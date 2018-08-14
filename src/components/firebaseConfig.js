4243f8a00bf34a55dc5ef95f0deeef4447f12dc5
Julia Gao Miller <julia.gao.miller@gmail.com>
2018-08-14 13:02:48 -0400

    Part 1 Complete
    In this video we will setup firestore, setup our app with Vue-CLI and create the Firebase config files.

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,12 @@
+import apikey from './apikey';
+
+//Note: keeping api key separate because am adding this to a public repo
+
+export default {
+    apiKey: apiKey,
+    authDomain: "traversy-vuejs-firestore.firebaseapp.com",
+    databaseURL: "https://traversy-vuejs-firestore.firebaseio.com",
+    projectId: "traversy-vuejs-firestore",
+    storageBucket: "traversy-vuejs-firestore.appspot.com",
+    messagingSenderId: "1013710705989"
+}
